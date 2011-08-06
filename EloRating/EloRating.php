21096e38d8506587f7ac29097a2a97aed220eeda
Priyesh Patel <priyesh@ukawala.com>
2011-08-06 12:14:07 +0100

    initial commit

---
+++ EloRating/EloRating.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * This class calculates ratings based on the Elo system used in chess.
+ *
+ * @author Priyesh Patel <priyesh@pexat.com>
+ * @copyright Copyright (c) 2011 onwards, Priyesh Patel
+ * @license Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License
+ */
+class Rating
+{
+
+    /**
+     * @var int The K Factor used.
+     */
+    const KFACTOR = 16;
+
+    /**
+     * Protected & private variables.
+     */
+    protected $_ratingA;
+
+    protected $_ratingB;
+
+    protected $_scoreA;
+
+    protected $_scoreB;
+
+    protected $_expectedA;
+
+    protected $_expectedB;
+
+    protected $_newRatingA;
+
+    protected $_newRatingB;
+
+    /**
+     * Costructor function which does all the maths and stores the results ready
+     * for retrieval.
+     *
+     * @param int Current rating of A
+     * @param int Current rating of B
+     * @param int Score of A
+     * @param int Score of B
+     */
+    public function  __construct($ratingA,$ratingB,$scoreA,$scoreB)
+    {
+        $this -> _ratingA = $ratingA;
+        $this -> _ratingB = $ratingB;
+        $this -> _scoreA = $scoreA;
+        $this -> _scoreB = $scoreB;
+
+        $expectedScores = $this -> _getExpectedScores($this -> _ratingA,$this -> _ratingB);
+        $this -> _expectedA = $expectedScores['a'];
+        $this -> _expectedB = $expectedScores['b'];
+
+        $newRatings = $this ->_getNewRatings($this -> _ratingA, $this -> _ratingB, $this -> _expectedA, $this -> _expectedB, $this -> _scoreA, $this -> _scoreB);
+        $this -> _newRatingA = $newRatings['a'];
+        $this -> _newRatingB = $newRatings['b'];
+    }
+
+    /**
+     * Retrieve the calculated data.
+     *
+     * @return Array An array containing the new ratings for A and B.
+     */
+    public function getNewRatings()
+    {
+        return array (
+            'a' => $this -> _newRatingA,
+            'b' => $this -> _newRatingB
+        );
+    }
+
+    /**
+     * Protected & private functions begin here
+     */
+
+    protected function _getExpectedScores($ratingA,$ratingB)
+    {
+        $expectedScoreA = 1 / ( 1 + ( pow( 10 , ( $ratingB - $ratingA ) / 400 ) ) );
+        $expectedScoreB = 1 / ( 1 + ( pow( 10 , ( $ratingA - $ratingB ) / 400 ) ) );
+
+        return array (
+            'a' => $expectedScoreA,
+            'b' => $expectedScoreB
+        );
+    }
+
+    protected function _getNewRatings($ratingA,$ratingB,$expectedA,$expectedB,$scoreA,$scoreB)
+    {
+        $newRatingA = $ratingA + ( self::KFACTOR * ( $scoreA - $expectedA ) );
+        $newRatingB = $ratingB + ( self::KFACTOR * ( $scoreB - $expectedB ) );
+
+        return array (
+            'a' => $newRatingA,
+            'b' => $newRatingB
+        );
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+#Elo Rating Class
+A PHP class which implements the [Elo rating system](http://en.wikipedia.org/wiki/Elo_rating_system).
+Written by Priyesh Patel
+
+<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/"><img alt="Creative Commons Licence" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-sa/3.0/88x31.png" /></a><br /><span xmlns:dct="http://purl.org/dc/terms/" property="dct:title">Elo Rating Class</span> by <a xmlns:cc="http://creativecommons.org/ns#" href="http://pexat.com/" property="cc:attributionName" rel="cc:attributionURL">Priyesh Patel</a> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/3.0/">Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License</a>.
